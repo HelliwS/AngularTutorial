d8c267ce9c06da1faf180556d496bb38d2b525ed
Steven Helliwell <stevehelliwell12@gmail.com>
2024-04-19 11:21:55 +0100

    testing threejs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { CanvasBoxComponent } from './components/canvas-box/canvas-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent,
+    CanvasBoxComponent
   ],
   imports: [
     BrowserModule,
